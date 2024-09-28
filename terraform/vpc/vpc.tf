d1f86ec285060e8fd076dbbe52bd63a6b3151881
jenya <jenya@jenyas-MacBook-Pro.local>
2024-09-28 20:52:46 +0300

    Remove .terraform directory from tracking

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,15 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs              = ["us-east-1a", "us-east-1b"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.101.0/24","10.0.102.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+
+  enable_nat_gateway  = true
+  enable_vpn_gateway  = false
+  single_nat_gateway  = true
+
+  create_database_subnet_group  = true
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Terraform = "true"
@@ -17,24 +31,6 @@ module "vpc" {
   }
 }
 
-resource "aws_eip" "nat" {
-  count = 3
-
-  vpc = true
-}
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  # The rest of arguments are omitted for brevity
-
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = "${aws_eip.nat.*.id}" # <= IPs specified here as input to the module
-  database_subnets    = ["10.0.21.0/24", "10.0.22.0/24"]
-elasticache_subnets = ["10.0.31.0/24", "10.0.32.0/24"]
-private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
-redshift_subnets    = ["10.0.41.0/24", "10.0.42.0/24"]
-intra_subnets       = ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]   
-single_nat_gateway = true
+output "vpc_id" {
+    value = module.vpc.vpc_id
 }
